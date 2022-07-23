edf59d769500747ba79917fb14ca2e2822fd9d85
Sagar Doshi <sagardoshi2020@gmail.com>
2022-07-23 20:54:27 +0530

    Solve lint issue

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ export const inflate = (token: string) => {
   return zlib.inflateSync(Buffer.from(token, 'base64')).toString(); //decompress
 };
 
-
 export const deflate = (token: string) => {
   return zlib.deflateSync(token).toString('base64'); //compress
 };
--- test/zlib.test.ts
+++ test/zlib.test.ts
@@ -2,10 +2,14 @@ import { inflate, deflate } from '../src';
 
 describe('Zlib Inflate and Inflate test', () => {
   it('Inflate Test Case', () => {
-    expect(inflate('eJwrTkxPLErJL87INDIwMnBIz03MzNFLzs8FAG5SCKE=')).toEqual('sagardoshi2020@gmail.com');
+    expect(inflate('eJwrTkxPLErJL87INDIwMnBIz03MzNFLzs8FAG5SCKE=')).toEqual(
+      'sagardoshi2020@gmail.com'
+    );
   });
 
   it('Deflate Test Case', () => {
-    expect(deflate('sagardoshi2020@gmail.com')).toEqual('eJwrTkxPLErJL87INDIwMnBIz03MzNFLzs8FAG5SCKE=');
-  })
+    expect(deflate('sagardoshi2020@gmail.com')).toEqual(
+      'eJwrTkxPLErJL87INDIwMnBIz03MzNFLzs8FAG5SCKE='
+    );
+  });
 });
